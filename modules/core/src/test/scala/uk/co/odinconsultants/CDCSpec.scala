b40685f6146d46115e5819253e56e96e09a19137
PhillipHenry <PhillHenry@gmail.com>
2023-11-29 11:46:10 +0000

    Working - sort of.

--- modules/core/src/test/scala/uk/co/odinconsultants/CDCSpec.scala
+++ modules/core/src/test/scala/uk/co/odinconsultants/CDCSpec.scala
@@ -1,23 +1,39 @@
 package uk.co.odinconsultants
-import org.apache.spark.sql.SaveMode
+import org.apache.spark.sql.{DataFrame, SaveMode, SparkSession}
 import org.scalatest.GivenWhenThen
 import uk.co.odinconsultants.documentation_utils.{Datum, SpecPretifier, TableNameFixture}
 
 class CDCSpec extends SpecPretifier with GivenWhenThen with TableNameFixture {
 
   "A dataset that is CDC enabled" should {
+    val sinkTable: String = "myDeltaTable"
     val tableName: String = Datum.getClass.getSimpleName.replace("$", "")
-    val createSql: String = s"""CREATE TABLE $tableName (id INT, label STRING, partitionKey LONG)
-                                      |USING DELTA
-                                      |""".stripMargin
-    val alterTableSql: String = s"ALTER TABLE $tableName SET TBLPROPERTIES (delta.enableChangeDataFeed = true)"
     "TODO do something" in new SimpleSparkFixture {
-      spark.sqlContext.sql(createSql)
-      spark.sqlContext.sql(alterTableSql)
-//      spark.createDataFrame(data).writeTo(tableName).append()
+      createTable(tableName, spark).show()
+      createTable(sinkTable, spark)
+      val deltaDF = spark.read
+        .format("delta")
+        .option("readChangeFeed", "true")
+        .option("startingVersion", 1)
+        .option("mergeSchema", "true")
+//        .option("endingVersion", 10)
+        .table(tableName)
+      spark.createDataFrame(data).writeTo(tableName).append()
       spark.createDataFrame(data).write.format("delta").mode(SaveMode.Append).saveAsTable(tableName)
+      deltaDF.show()
+//      deltaDF.writeTo(sinkTable).append()
       // TODO
     }
   }
 
+  private def createTable(tableName: String, spark: SparkSession): DataFrame = {
+    val createSql: String = s"""CREATE TABLE $tableName (id INT, label STRING, partitionKey LONG)
+                                      |USING DELTA
+                                      |""".stripMargin
+    val alterTableSql: String =
+      s"ALTER TABLE $tableName SET TBLPROPERTIES (delta.enableChangeDataFeed = true)"
+    spark.sqlContext.sql(createSql)
+    spark.sqlContext.sql(alterTableSql)
+    spark.sqlContext.sql(s"DESCRIBE HISTORY $tableName")
+  }
 }
