f7f6f68f9b57f894a4863ae74731fab326b3c970
PhillipHenry <PhillHenry@gmail.com>
2023-12-06 13:58:11 +0000

    Metastore tests.

--- modules/core/src/test/scala/uk/co/odinconsultants/CrudSpec.scala
+++ modules/core/src/test/scala/uk/co/odinconsultants/CrudSpec.scala
@@ -1,4 +1,5 @@
 package uk.co.odinconsultants
+import org.apache.spark.sql.catalyst.TableIdentifier
 import org.scalatest.GivenWhenThen
 import uk.co.odinconsultants.documentation_utils.{SpecPretifier, TableNameFixture}
 
@@ -9,6 +10,13 @@ class CrudSpec extends SpecPretifier with GivenWhenThen with TableNameFixture {
       val sinkSQL = createTableSQLUsingDelta(tableName)
       Given(s"a table created with SQL${formatSQL(sinkSQL)}")
       spark.sqlContext.sql(sinkSQL)
+      When(s"we write ${data.length} rows to $tableName")
+      appendData(tableName)
+      Then("the table indeed contains all the data")
+      assertDataIn(tableName)
+      And(s"the metastore contains a reference to the table $tableName")
+      assert(spark.sessionState.catalog.tableExists(TableIdentifier(tableName)))
+      assert(spark.sessionState.catalog.externalCatalog.getTable("default", tableName) != null)
     }
   }
 
